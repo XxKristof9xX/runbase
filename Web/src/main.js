a9f4e9b249359bf566a17eabcc0f73669e6c96f3
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-02-28 12:38:48 +0100

    alapértelmezett oldal beállítása

--- Web/src/main.js
+++ Web/src/main.js
@@ -4,5 +4,9 @@ import router from './router'
 import "./assets/style.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import axios from "axios";
+
+axios.defaults.baseURL = "https://runbase.hu";
+
 createApp(App).use(router).mount('#app')
 import "bootstrap/dist/js/bootstrap.js"
