1aa30c8165be74fe1ca4572951c75b9ef6e52ada
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-02-28 13:08:42 +0100

    api átírása

--- Web/vue.config.js
+++ Web/vue.config.js
@@ -1,14 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-});
+})
 
 module.exports = {
   devServer: {
     proxy: {
-      "/api": {
+      "^/api": { 
         target: "https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net",
         changeOrigin: true,
+        secure: false, 
+        logLevel: "debug", 
       },
     },
   },
