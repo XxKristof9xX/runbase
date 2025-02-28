81387a22e636c610378fb2970a54a79d8a5f689a
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-02-28 13:01:19 +0100

    api hivás tárolása vue.configba

--- Web/vue.config.js
+++ Web/vue.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-})
+});
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net",
+        changeOrigin: true,
+      },
+    },
+  },
+};
 
 
