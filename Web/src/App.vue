ab7bfa91395e2bafccd70ded31c9638b232ecbb3
Grimshaw Kristóf <sziaxbox360@gmail.com>
2025-03-02 11:54:30 +0100

    bejelentkezés után profil és kijelntkezés gomb megjelenik a nav bar-ban

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,115 +1,134 @@
 <template>
-  <!-- Navigáció -->
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <router-link to="/" class="navbar-brand"><img src="../Images/RunBase_full_logo.png" alt=""></router-link>
-      <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span class="navbar-toggler-icon" onclick="navbarMenu()">
-          <div id="myLinks" class="navbar-nav">
-            <router-link to="/" class="nav-item nav-link active">Főoldal</router-link>
-            <router-link to="/competition" class="nav-item nav-link">Versenyek</router-link>
-            <router-link to="/result" class="nav-item nav-link">Eredmények</router-link>
-            <router-link to="/contact" class="nav-item nav-link">Kapcsolat</router-link>
+  <div id="app">
+    <!-- Navigációs sáv -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">
+          <img src="../Images/RunBase_full_logo.png" alt="RunBase Logo" />
+        </router-link>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <div class="navbar-nav">
+            <router-link to="/" class="nav-item nav-link">Főoldal</router-link>
+            <router-link to="/versenyek" class="nav-item nav-link">Versenyek</router-link>
+            <router-link to="/eredmenyek" class="nav-item nav-link">Eredmények</router-link>
+            <router-link to="/kapcsolat" class="nav-item nav-link">Kapcsolat</router-link>
           </div>
-        </span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav">
-          <router-link to="/" class="nav-item nav-link active">Főoldal</router-link>
-          <router-link to="/versenyek" class="nav-item nav-link">Versenyek</router-link>
-          <router-link to="/eredmenyek" class="nav-item nav-link">Eredmények</router-link>
-          <router-link to="/kapcsolat" class="nav-item nav-link">Kapcsolat</router-link>
-        </div>
-        <div class="navbar-nav ms-auto">
-          <button id="login" @click="toggleLogin">
-            <router-link v-if="!isLoggedIn" to="/login" class="nav-item nav-link">Bejelentkezés</router-link>
-            <router-link v-else @click="logout" class="nav-item nav-link">Kijelentkezés</router-link>
-          </button>
+          <div class="navbar-nav ms-auto">
+            <template v-if="isLoggedIn">
+              <router-link to="/profil" class="nav-item nav-link">Profil</router-link>
+              <button class="btn btn-danger nav-item nav-link" @click="logout">Kijelentkezés</button>
+            </template>
+            <router-link v-else to="/login" class="nav-item nav-link">Bejelentkezés</router-link>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-  <router-view/>
+    </nav>
+    
+    <!-- Tartalom -->
+    <router-view />
+  </div>
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
+
 export default {
-  data() {
-    return {
-      isLoggedIn: false, // Felhasználói állapot
+  setup() {
+    const isLoggedIn = ref(false);
+
+    // Bejelentkezési állapot ellenőrzése
+    const checkLoginStatus = () => {
+      isLoggedIn.value = !!sessionStorage.getItem("user");
     };
-  },
-  methods: {
-    toggleLogin() {
-      this.isLoggedIn = !this.isLoggedIn;
-    },
-    logout() {
-      this.isLoggedIn = false;
-      localStorage.removeItem("user");
-      this.$router.push("/login");
-    },
-  },
-  created() {
-    const user = localStorage.getItem("user");
-    if (user) {
-      this.isLoggedIn = true;
-    }
+
+    // Frissítés nélküli állapotváltás figyelése
+    const updateLoginStatus = () => {
+      checkLoginStatus();
+    };
+
+    // Login állapot frissítése eseményfigyelővel
+    window.addEventListener("loginStatusChanged", updateLoginStatus);
+
+    // Indításkor ellenőrizzük az állapotot
+    onMounted(checkLoginStatus);
+
+    // Kijelentkezés
+    const logout = () => {
+      sessionStorage.removeItem("user");
+      checkLoginStatus();
+      window.dispatchEvent(new Event("loginStatusChanged")); // Fejléc frissítése
+    };
+
+    return { isLoggedIn, logout };
   },
 };
 </script>
 
-<style>
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-  
-  body{
-    background-color: #ADD8E6;
-  }
-  nav {
-    /* padding: 30px; */
-    background-color: #20283F;
-    justify-content: flex-start;
-  }
-  .navbar {
-    /* padding: 30px; */
-    /* color: #20283F; */
-  }
-  
-  nav a {
-    color: rgb(10, 139, 190) !important;
-    text-decoration: none; /* Aláhúzás eltávolítása */
-    transition: color 0.3s; /* Színátmenet animáció */
-  }
-  
-  nav a.router-link-exact-active {
-    color: white !important;
-    font-weight: bold; /* (opcionális) Vastagabb betű */
-  }
-  
-  nav a:hover {
-    color: #00bfff; /* Hover szín */
-  }
-  
-  img{
-    /* height: 6%; */
-    width: 7em;
-    /* margin: 0 auto; */
-    margin-bottom: 13px;
-    float: left;
-  }
-  .navbar ul li {
-    float: left;
-  }
-  
-  #login{
-    border: 2px solid white;
-    border-radius: 5px;
-    background-color: #6F8FAF;
-  }
-  </style>
+<style>
+/* Globális beállítások */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+/* Háttérszín */
+body {
+  background-color: #ADD8E6;
+}
+
+/* Navigáció */
+nav {
+  background-color: #20283F;
+}
+
+.navbar-nav {
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  color: rgb(10, 139, 190) !important;
+  text-decoration: none;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active {
+  color: white !important;
+  font-weight: bold;
+}
+
+nav a:hover {
+  color: #00bfff;
+}
+
+/* Logo */
+img {
+  width: 7em;
+  margin-bottom: 13px;
+  float: left;
+}
+
+/* Bejelentkezés/Kijelentkezés gomb */
+#login {
+  border: 2px solid white;
+  border-radius: 5px;
+  background-color: #6F8FAF;
+}
+
+/* Gombok igazítása */
+.nav-item.nav-link {
+  padding: 10px 15px;
+  border-radius: 5px;
+}
+
+.btn-danger {
+  margin-left: 10px;
+}
+</style>
--- Web/src/views/DataPages/LoginPage.vue
+++ Web/src/views/DataPages/LoginPage.vue
@@ -5,50 +5,27 @@
       <form @submit.prevent="login">
         <!-- Felhasználónév input -->
         <div class="form-outline mb-4">
-          <input
-            type="text"
-            id="username"
-            class="form-control"
-            v-model="form.username"
-            required
-          />
+          <input type="text" id="username" class="form-control" v-model="form.username" required />
           <label class="form-label" for="username">Felhasználónév</label>
         </div>
 
         <!-- Jelszó input -->
         <div class="form-outline mb-4">
-          <input
-            type="password"
-            id="password"
-            class="form-control"
-            v-model="form.password"
-            required
-          />
+          <input type="password" id="password" class="form-control" v-model="form.password" required />
           <label class="form-label" for="password">Jelszó</label>
         </div>
 
         <!-- "Emlékezzen rám" checkbox -->
         <div class="form-check mb-4">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            id="rememberMe"
-            v-model="form.remember"
-          />
-          <label class="form-check-label" for="rememberMe">
-            Emlékezzen rám
-          </label>
+          <input class="form-check-input" type="checkbox" id="rememberMe" v-model="form.remember" />
+          <label class="form-check-label" for="rememberMe">Emlékezzen rám</label>
         </div>
 
         <!-- Hibaüzenet (ha van) -->
-        <div v-if="errorMessage" class="alert alert-danger">
-          {{ errorMessage }}
-        </div>
+        <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
 
         <!-- Bejelentkezés gomb -->
-        <button type="submit" class="btn btn-primary btn-block mb-4">
-          Bejelentkezés
-        </button>
+        <button type="submit" class="btn btn-primary btn-block mb-4">Bejelentkezés</button>
       </form>
     </div>
   </div>
@@ -65,14 +42,12 @@ export default {
         password: "",
         remember: false,
       },
-      errorMessage: "", 
+      errorMessage: "",
     };
   },
   methods: {
   async login() {
     try {
-      console.log("Elküldött adatok:", this.form.username, this.form.password);
-
       const response = await axios.post(
         "https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net/api/felhasznalok/login",
         {
@@ -86,21 +61,19 @@ export default {
         }
       );
 
-      console.log("API teljes válasz:", response);
-      console.log("API válasz (data):", response.data);
-
       if (typeof response.data === "string" && response.data.includes("Sikeres")) {
         window.alert("Sikeres bejelentkezés!");
 
+        // Felhasználó mentése és esemény küldése
+        sessionStorage.setItem("user", this.form.username);
+        window.dispatchEvent(new Event("loginStatusChanged")); // Fejléc frissítése
+        
         this.$router.push("/");
-        this.$root.$emit("loginStatusChanged", true);
       } else {
         this.errorMessage = "Helytelen felhasználónév vagy jelszó!";
       }
     } catch (error) {
-      console.error("Bejelentkezési hiba:", error.response);
-      this.errorMessage =
-        error.response?.data || "Hiba történt a bejelentkezés során!";
+      this.errorMessage = error.response?.data || "Hiba történt a bejelentkezés során!";
     }
   },
 },
