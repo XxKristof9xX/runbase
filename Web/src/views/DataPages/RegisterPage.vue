c2eca14e185aba72ae98bddfebe11458dfb86172
Grimshaw Kristóf <h157974@stud.u-szeged.hu>
2025-03-03 13:09:14 +0100

    registerpage létrehozva

---
+++ Web/src/views/DataPages/RegisterPage.vue
@@ -0,0 +1,74 @@
+// src/views/DataPages/RegisterPage.vue
+<template>
+  <h1 class="my-3">Regisztráció</h1>
+  <div class="container">
+    <div class="row">
+      <form @submit.prevent="register">
+        <div class="form-outline mb-4">
+          <input type="text" id="username" class="form-control" v-model="form.username" required />
+          <label class="form-label" for="username">Felhasználónév</label>
+        </div>
+
+        <div class="form-outline mb-4">
+          <input type="password" id="password" class="form-control" v-model="form.password" required />
+          <label class="form-label" for="password">Jelszó</label>
+        </div>
+
+        <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
+        <div v-if="successMessage" class="alert alert-success">{{ successMessage }}</div>
+
+        <button type="submit" class="btn btn-primary btn-block mb-4">Regisztráció</button>
+        <router-link to="/login" class="btn btn-secondary">Vissza a bejelentkezéshez</router-link>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+        email: "",
+      },
+      errorMessage: "",
+      successMessage: "",
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        const response = await axios.post(
+          "https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net/api/felhasznalok/",
+          {
+            Nev: this.form.username,
+            Jelszo: this.form.password,
+          },
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+
+        if (response.status === 201) {
+          this.successMessage = "Sikeres regisztráció! Most bejelentkezhetsz.";
+          setTimeout(() => this.$router.push("/login"), 2000);
+        } else {
+          this.errorMessage = "Hiba történt a regisztráció során!";
+        }
+      } catch (error) {
+        this.errorMessage = error.response?.data || "Hiba történt a regisztráció során!";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  font-size: 3em;
+}
+</style>
