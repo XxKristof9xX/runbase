98751975a137136d722e8127e7467675db0183e3
Grimshaw Kristóf <sziaxbox360@gmail.com>
2025-05-10 22:16:19 +0200

    verseny adatainak megnyitása a kártyára kattintva

--- Web/src/views/DataPages/CompetitionPage.vue
+++ Web/src/views/DataPages/CompetitionPage.vue
@@ -1,42 +1,24 @@
 <template>
   <h1 class="my-3 text-center">Versenyek</h1>
   <v-container>
-    <v-row justify="center" v-if="loading">
-      <v-col v-for="i in 6" :key="i" cols="12" sm="6" md="4" lg="3">
-        <v-card>
-          <v-card-title class="text-center">
-            <v-skeleton-loader type="text"></v-skeleton-loader>
-          </v-card-title>
-          <v-card-text class="d-flex justify-center">
-            <v-skeleton-loader type="image"></v-skeleton-loader>
-          </v-card-text>
-          <v-card-actions class="justify-center">
-            <v-skeleton-loader type="button"></v-skeleton-loader>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center" v-else>
+    <v-row justify="center">
       <v-col v-for="c in competitions" :key="c.id" cols="12" sm="6" md="4" lg="3">
-        <v-card>
+        <v-card class="hoverable" :to="{
+          name: 'CompetitionDetails',
+          params: { nev: c.nev },
+          query: {
+            id: c.versenyId,
+            helyszin: c.helyszin,
+            datum: c.datum,
+            leiras: c.leiras,
+            max_letszam: c.maxLetszam,
+            kep: c.kep,
+          },
+        }">
           <v-img v-if="c.kep" :src="getImage(c.kep)" cover></v-img>
           <v-card-title class="text-center">{{ c.nev }}</v-card-title>
           <v-card-actions>
-            <v-btn
-              color="primary" 
-              :to="{
-                name: 'CompetitionDetails',
-                params: { nev: c.nev },
-                query: {
-                  id: c.versenyId,
-                  helyszin: c.helyszin,
-                  datum: c.datum,
-                  leiras: c.leiras,
-                  max_letszam: c.maxLetszam,
-                  kep: c.kep,
-                },
-              }"
-            >
+            <v-btn color="primary">
               További információk
             </v-btn>
           </v-card-actions>
@@ -46,6 +28,7 @@
   </v-container>
   <router-view />
 </template>
+
 
 <script>
 import axios from "axios";
@@ -72,10 +55,10 @@ export default {
       });
   },
   methods: {
-  getImage(base64Data) {
-    return `data:image/jpeg;base64,${base64Data}`;
+    getImage(base64Data) {
+      return `data:image/jpeg;base64,${base64Data}`;
+    }
   }
-}
 };
 </script>
 <style>
