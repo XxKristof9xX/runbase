d9f4433cac6973721b50aa973d045074f4d19c98
Grimshaw Kristóf <sziaxbox360@gmail.com>
2025-05-11 20:12:05 +0200

    profil oldal frissítése

--- Web/src/views/DataPages/ProfilePage.vue
+++ Web/src/views/DataPages/ProfilePage.vue
@@ -12,17 +12,38 @@
       </div>
 
       <div v-if="!user.competitorId">
-        <h5 class="mt-3">Versenyző azonosítása</h5>
-        <input v-model="tajszam" type="text" class="form-control mt-2" placeholder="TAJ szám megadása" />
-        <button class="btn btn-success mt-2" @click="identifyCompetitor">Azonosítás</button>
+        <div class="text-center mt-4">
+          <h5>
+            {{ ujVersenyzo ? "Új versenyzői adatok megadása" : "Már regisztrált versenyző hozzárendelése" }}
+          </h5>
+          <div class="form-check form-switch d-inline-block mt-2" style="transform: scale(1.8);">
+            <input class="form-check-input" type="checkbox" id="ujVersenyzoSwitch" v-model="ujVersenyzo">
+          </div>
+        </div>
+
+
+        <div v-if="ujVersenyzo">
+          <input v-model="newCompetitor.nev" type="text" class="form-control mt-3" placeholder="Név" />
+          <input v-model="newCompetitor.szuletesiEv" type="number" class="form-control mt-2"
+            placeholder="Születési év" />
+          <select v-model="newCompetitor.neme" class="form-select mt-2">
+            <option disabled value="">Neme kiválasztása</option>
+            <option value="ferfi">Férfi</option>
+            <option value="no">Nő</option>
+          </select>
+          <input v-model="newCompetitor.tajSzam" type="text" class="form-control mt-2" placeholder="TAJ szám" />
+          <button class="btn btn-success mt-3" @click="createAndAssignCompetitor">Létrehozás</button>
+        </div>
+        <div v-else>
+          <input v-model="tajszam" type="text" class="form-control mt-2" placeholder="TAJ szám megadása" />
+          <button class="btn btn-success mt-2" @click="identifyCompetitor">Azonosítás</button>
+        </div>
         <p v-if="message" :class="{ 'text-success': success, 'text-danger': !success }">{{ message }}</p>
       </div>
-
       <button v-if="user.competitorId" type="button" class="btn btn-primary mt-3" data-bs-toggle="modal"
         data-bs-target="#exampleModal6" @click="fetchCompetitorData">
         Leírás
       </button>
-
       <button class="btn btn-danger mt-3 ms-2" @click="logout">Kijelentkezés</button>
 
       <div class="modal fade bd-example-modal-lg" id="exampleModal6" tabindex="-1" aria-labelledby="exampleModalLabel6"
@@ -94,6 +115,9 @@
 </template>
 
 
+
+
+
 <script>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
@@ -108,6 +132,13 @@ export default {
     const tajszam = ref("");
     const message = ref("");
     const success = ref(false);
+    const ujVersenyzo = ref(false);
+    const newCompetitor = ref({
+      nev: "",
+      szuletesiEv: "",
+      neme: "",
+      tajSzam: "",
+    });
 
     const loadUserData = () => {
       const storedUser = sessionStorage.getItem("user");
@@ -263,6 +294,68 @@ export default {
       }
     };
 
+    const createAndAssignCompetitor = async () => {
+      const adat = newCompetitor.value;
+
+      if (!adat.nev || !adat.szuletesiEv || !adat.neme || !adat.tajSzam) {
+        message.value = "Kérlek, töltsd ki az összes mezőt!";
+        success.value = false;
+        return;
+      }
+
+      try {
+        const createResponse = await axios.post(
+          "https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net/api/versenyzo/hozzaad",
+          {
+            nev: adat.nev,
+            szuletesiEv: adat.szuletesiEv,
+            neme: adat.neme,
+            tajSzam: adat.tajSzam,
+          }
+        );
+        if (createResponse.status === 200) {
+          const getIdResponse = await axios.get(
+            `https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net/api/versenyzo/getByTaj/${adat.tajSzam}`
+          );
+          const versenyzoId = getIdResponse.data.versenyzoId;
+          const assignResponse = await axios.put(
+            "https://runbaseapi-e7avcnaqbmhuh6bp.northeurope-01.azurewebsites.net/api/versenyzo/addVersenyzo",
+            {
+              tajSzam: adat.tajSzam,
+              felhasznaloId: user.value.id,
+            }
+          );
+          if (assignResponse.status === 200) {
+            user.value.competitorId = versenyzoId;
+            user.value.type = "competitor";
+
+            const updatedUser = {
+              id: user.value.id,
+              nev: user.value.username,
+              tipus: "competitor",
+              versenyzoId: versenyzoId,
+              apiKey: user.value.apiKey,
+            };
+            sessionStorage.setItem("user", JSON.stringify(updatedUser));
+            message.value = "Versenyző sikeresen létrehozva és hozzárendelve!";
+            success.value = true;
+            newCompetitor.value = { nev: "", szuletesiEv: "", neme: "", tajSzam: "" };
+          } else {
+            message.value = "Hiba a hozzárendelés során.";
+            success.value = false;
+          }
+        } else {
+          message.value = "Hiba történt a létrehozás során.";
+          success.value = false;
+        }
+      } catch (error) {
+        console.error("Hiba:", error);
+        message.value = error.response?.data?.message || "Ismeretlen hiba történt.";
+        success.value = false;
+      }
+    };
+
+
     const logout = () => {
       sessionStorage.removeItem("user");
       window.dispatchEvent(new Event("loginStatusChanged"));
@@ -283,6 +376,9 @@ export default {
       success,
       statistics,
       calculateStatistics,
+      ujVersenyzo,
+      newCompetitor,
+      createAndAssignCompetitor,
     };
   },
 };
